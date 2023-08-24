479d3362b71a7d21821b3adcb40d5c0f1c75e3aa
mostaeen <abdullah.mostaeen@gmail.com>
2023-08-24 12:46:14 +0600

    'models-added'

--- company/models.py
+++ company/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Employee(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
--- device/models.py
+++ device/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from company.models import Employee
 
 # Create your models here.
+
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class DeviceLog(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    checked_out = models.DateTimeField()
+    returned = models.DateTimeField()
+    condition_out = models.TextField()
+    condition_returned = models.TextField()
