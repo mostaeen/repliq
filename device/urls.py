3d8d4b3331b1a0cf698586e5117796ab52554806
mostaeen <abdullah.mostaeen@gmail.com>
2023-08-24 15:04:18 +0600

    'device-update-url'

--- device/urls.py
+++ device/urls.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 router.register(r'devices', DeviceViewSet)
 
 # API available at http://127.0.0.1:8000/gears/devicelogs/
+# For device update: http://127.0.0.1:8000/gears/devicelogs/1  //// Here 1 is devicelog id
 router.register(r'devicelogs', DeviceLogViewSet)
 
 urlpatterns = router.urls
